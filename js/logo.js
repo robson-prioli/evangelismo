a8e5e1dfbffb14fdc8f65d0ce2fa767efcdd7d45
robs-full <159490209+robs-full@users.noreply.github.com>
2024-06-24 22:37:45 -0300

    first commit

--- js/logo.js
+++ js/logo.js
@@ -3,7 +3,7 @@ const screens = document.querySelectorAll('.screen');
 const changeScreen = async() => {
     screens[0].classList.add('up');
 
-    setTimeout(() =>new TypeIt(".box-admirer", { speed: 150, startDelay: 3500, loop: true, loopDelay: 500 }), 3000);
+    setTimeout(() =>new TypeIt(".box-admirer", { speed: 150, startDelay: 1500, loop: true, loopDelay: 500 }), 3000);
 }
 
 window.addEventListener('load', () => setTimeout(() => changeScreen(), 2000));
