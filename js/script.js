09fcab6e814b11e1cd89c2256e81a50d266cf26c
robs-full <159490209+robs-full@users.noreply.github.com>
2024-07-13 10:19:15 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -36,7 +36,8 @@ const analytics = async(btnClick = false) => {
     try {
         let data = new FormData();
         data.append('btnClick', btnClick);
-        data.append('action', ((btnClick) ? 'btn' : 'view'));
+        data.append('action', ((btnClick) ? 'btn' : 'view')); 
+        data.append('screen', `${window.screen.width}x${window.screen.height}`);
         data.append('callback', 'evangelismo');
 
         await fetch('https://robson-prioli.com/conviva/src/ajax/evangelismo.ajax.php', {method: 'POST', body: data });
