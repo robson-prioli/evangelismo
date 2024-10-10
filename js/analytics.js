1ed4d12d578e0b0dc422bfa923a883118aef208f
Robson Prioli <robsonprioli.ti@gmail.com>
2024-10-10 18:15:26 -0300

    fix __book

--- js/analytics.js
+++ js/analytics.js
@@ -5,7 +5,7 @@ const analytics = async(btnClick = false) => {
         data.append('btnClick', btnClick);
         data.append('action', ((btnClick) ? 'btn' : 'view')); 
         data.append('screen', `${window.screen.width}x${window.screen.height}`);
-        data.append('book', `${__book}`);
+        data.append('book', `${__book ?? ''}`);
         data.append('church', `${document.body.getAttribute('data-church')}`);
         data.append('callback', 'evangelismo');
 
