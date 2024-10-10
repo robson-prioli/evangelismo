39be6e8942bf054c1d0aba5762eb0b5bad917bfa
Robson Prioli <robsonprioli.ti@gmail.com>
2024-10-10 18:11:17 -0300

    fix analytics churchs

---
+++ js/analytics.js
@@ -0,0 +1,23 @@
+
+const analytics = async(btnClick = false) => {
+    try {
+        let data = new FormData();
+        data.append('btnClick', btnClick);
+        data.append('action', ((btnClick) ? 'btn' : 'view')); 
+        data.append('screen', `${window.screen.width}x${window.screen.height}`);
+        data.append('book', `${__book}`);
+        data.append('church', `${document.body.getAttribute('data-church')}`);
+        data.append('callback', 'evangelismo');
+
+        await fetch('https://robson-prioli.com/conviva/src/ajax/evangelismo.ajax.php', {method: 'POST', body: data });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+if(document.querySelector('.btnClick')){
+    document.querySelector('.btnClick').addEventListener('click', () => analytics(true));
+}
+
+window.addEventListener('load', async () => await Promise.all([await analytics()]));
--- js/script.js
+++ js/script.js
@@ -39,24 +39,4 @@ const catchVerse = async() => {
     boxVerse.innerHTML = `error`;
 }
 
-const analytics = async(btnClick = false) => {
-    try {
-        let data = new FormData();
-        data.append('btnClick', btnClick);
-        data.append('action', ((btnClick) ? 'btn' : 'view')); 
-        data.append('screen', `${window.screen.width}x${window.screen.height}`);
-        data.append('book', `${__book}`);
-        data.append('callback', 'evangelismo');
-
-        await fetch('https://robson-prioli.com/conviva/src/ajax/evangelismo.ajax.php', {method: 'POST', body: data });
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-if(document.querySelector('.btnClick')){
-    document.querySelector('.btnClick').addEventListener('click', () => analytics(true));
-}
-
-window.addEventListener('load', async () => await Promise.all([await loadVerses(), await analytics()]));
+window.addEventListener('load', async () => await Promise.all([await loadVerses()]));
